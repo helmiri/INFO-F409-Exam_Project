047d73bd5977b2fec96a04087d1850466921d989
Julien Baudru <julien.baudru@gmail.com>
2021-12-28 10:42:57 +0100

    squelette class

---
+++ agent.py
@@ -0,0 +1,16 @@
+
+class Agent:
+    def __init__(self, learning_rate=0.5, aspiration=2, habituation=0, probab_init=0.5):
+        pass
+
+    def learn(self, stimulus, action):
+        pass
+
+    def updt_aspi(self, payoff):
+        pass
+
+    def act(self):
+        pass
+
+    def cmpt_stimu(self, payoff, supremum):
+        pass
